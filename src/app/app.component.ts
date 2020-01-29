09762989781235364886fec20ac0fb72edda5b9e
Lance Finney <lance.finney@oasisdigital.com>
2020-01-29 09:35:49 -0600

    Initial WebGL code
    Based on https://github.com/JohnnyDevNull/ng-three-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { EngineService } from './engine.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'three-test';
+export class AppComponent implements OnInit {
+
+  @ViewChild('rendererCanvas', { static: true })
+  rendererCanvas: ElementRef<HTMLCanvasElement>;
+
+  constructor(private engServ: EngineService) { }
+
+  ngOnInit() {
+    this.engServ.createScene(this.rendererCanvas);
+    this.engServ.animate();
+  }
+
 }
